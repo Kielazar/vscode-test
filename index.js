589f24d7b152e710f75b3bbc4016c2727838a79c
Kielazar (Jez) <magbanua.jezreel.m@gmail.com>
2025-04-17 16:03:00 +0800

    Added some files
    I added an two html files with one of em having a js

---
+++ index.js
@@ -0,0 +1,18 @@
+let age = 20;
+let inches = 6;
+let height = 180;
+
+let firstName = "Kielazar";
+let lastName = "Magbanua";
+
+console.log(`Your age is ${age}.`);
+console.log(`Your d size is ${inches}.`);
+console.log(`Your height is ${height}.`);
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName} ${lastName}.`)
+
+document.getElementById("p1").textContent = (`Your name is ${firstName} ${lastName}`);
+document.getElementById("p2").textContent = (`Your age is ${age}.`);
+document.getElementById("p3").textContent = (`Your Dick Length is ${inches} inches.`);
+
